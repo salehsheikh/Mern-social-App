582c72828f21d89e75c8f068599fdde500f25eea
saleh sheikh <salehsheikh786@gmail.com>
2024-03-10 23:44:56 +0500

    adding post logic

---
+++ backend/controllers/user.js
@@ -0,0 +1,57 @@
+import User from "../models/User.js";
+
+export const getUser=async(req,res)=>{
+    try{
+        const{id}=req.params;
+        const user=await User.findById(id);
+        res.status(200).json(user)
+;    }catch(err){
+    res.status(400).json({message:err.message});
+}
+}
+
+export const getUeserFriends=async(req,res)=>{
+    try {
+        const {id}=req.params;
+        const user=await User.findById(id);
+        const friends=await Promise.all(
+            user.friends.map((id)=>User.findById(id))
+        );
+        const formattedFriends=friends.map((
+            {id,firstname,lastName,occupation,location,picturePath} 
+            )=>{
+                return {id,firstname,lastName,occupation,location,picturePath} 
+            });
+            res.status(200).json(formattedFriends);
+    } catch (err) {
+        res.status(400).json({message:err.message});
+    }
+}
+
+export const addRemoveFriend=async(req,res)=>{
+    try{
+        const {id,friendId}=req.params;
+        const user=User.findById(id);
+        const friend=await User.findById(friendId);
+        if(user.friendId.includes(friendId)){
+            user.friends=user.friends.filter((id)=>id!==friendId);
+            friend.friends=friend.friend.filter((id)=>id!==id);
+        }else{
+            user.friends.push(friendId);
+            friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+    const friends=await Promise.all(
+        user.friends.map((id)=>User.findById(id))
+    );
+    const formattedFriends=friends.map((
+        {id,firstname,lastName,occupation,location,picturePath} 
+        )=>{
+            return {id,firstname,lastName,occupation,location,picturePath} 
+        });
+        res.status(200).json(formattedFriends);
+    }catch(err){
+        res.status(400).json({message:err.message});
+    }
+}
