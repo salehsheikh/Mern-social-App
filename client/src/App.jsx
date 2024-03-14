48666df5f1db8d51c1e0aff762942e743a9be8d7
saleh sheikh <salehsheikh786@gmail.com>
2024-03-15 01:34:48 +0500

    updating App with new route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,38 @@
-import React, { useMemo } from 'react'
-import { Routes ,BrowserRouter,Navigate,Route} from 'react-router-dom'
-import { themeSettings } from '../theme'
-import { useSelector } from 'react-redux'
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { CssBaseline ,ThemeProvider} from '@mui/material'
-import Home from './scenes/homePage'
+import { themeSettings } from "./theme";
+import HomePage from "./scenes/homePage";
 
-const App = () => {
-  const mode=useSelector((state)=>state.mode);
-  const theme=useMemo(()=>createTheme(themeSettings(mode)),[mode] );
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className='app'>
-     < BrowserRouter>
-  < ThemeProvider theme={theme}> 
-  <CssBaseline/>
- 
-  <Routes>
-      <Route path='/' element={<Home/>}/>
-      </Routes>
-      </ThemeProvider>
-      </ BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
